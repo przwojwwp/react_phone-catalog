9104e0fbd23f59bd422272554dbe318495345a52
wojciechprzyluski <przyluskiwojciech@gmail.com>
2025-04-11 05:47:34 +0200

    feat: introduce useIsMobile

---
+++ src/hooks/useIsMobile.ts
@@ -0,0 +1,15 @@
+import { useEffect, useState } from 'react';
+
+export const useIsMobile = (breakpoint = 639) => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const check = () => setIsMobile(window.innerWidth <= breakpoint);
+    check();
+
+    window.addEventListener('resize', check);
+    return () => window.removeEventListener('resize', check);
+  }, [breakpoint]);
+
+  return isMobile;
+};
