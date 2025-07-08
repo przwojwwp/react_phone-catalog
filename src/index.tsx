099a62b31106b77bc4901c4439da4881cef30257
wojciechprzyluski <przyluskiwojciech@gmail.com>
2025-07-08 15:08:40 +0200

    ref: use variable instead of hardcoded color

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
 import { HashRouter as Router } from 'react-router-dom';
-import './styles/main.scss';
 import { MobileMenuProvider } from './modules/shared/context/MobileMenuContext';
+
+import './styles/main.scss';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <MobileMenuProvider>
