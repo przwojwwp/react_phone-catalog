e8628ea7854617800c6549eef63a66f991496d99
wojciechprzyluski <przyluskiwojciech@gmail.com>
2025-07-12 08:29:58 +0200

    fix: mobileMenu animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
 import { Header } from './modules/shared/components/Header';
 import { Footer } from './modules/shared/components/Footer/Footer';
 import { MobileMenu } from './modules/shared/components/MobileMenu';
-import { useMobileMenu } from './modules/shared/context/MobileMenuContext';
 
 import './App.scss';
 
 export const App = () => {
-  const { isOpen } = useMobileMenu();
-
   return (
     <div className="App">
-      {isOpen ? <MobileMenu /> : <Header />}
+      <MobileMenu />
+      <Header />
       {/* <h1>Product Catalog</h1> */}
 
       <Footer />
