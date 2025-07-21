85e858f229c53d284167cdde6aec63deb626016d
wojciechprzyluski <przyluskiwojciech@gmail.com>
2025-07-21 16:09:28 +0200

    feat: infinity carousel

--- src/modules/HomePage/HomePage.tsx
+++ src/modules/HomePage/HomePage.tsx
@@ -9,16 +9,6 @@ export const HomePage = () => {
       <h1 className="visually-hidden">Product Catalog</h1>
 
       <PictureSlider />
-
-      {/* <div className={styles['banner-carousel-container']}>
-        <div className={styles['banner-carousel-track']}>
-          <div className={styles['banner-carousel-cell']}>1</div>
-          <div className={styles['banner-carousel-cell']}>2</div>
-          <div className={styles['banner-carousel-cell']}>3</div>
-        </div>
-        <button className={styles['banner-carousel-button prev']}>←</button>
-        <button className={styles['banner-carousel-button next']}>→</button>
-      </div> */}
     </>
   );
 };
--- src/modules/HomePage/components/PictureSlider.tsx
+++ src/modules/HomePage/components/PictureSlider.tsx
@@ -5,20 +5,32 @@ import styles from './PictureSlider.module.scss';
 
 const images = [
   '/img/banner-home-page-1.png',
-  '/img/banner-home-page-1.png',
-  '/img/banner-home-page-1.png',
+  '/img/cart-is-empty.png',
+  '/img/page-not-found.png',
 ];
+
+const adjustedImages = [...images, '/img/banner-home-page-1.png'];
 
 export const PictureSlider = () => {
   const [index, setIndex] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setIndex(prev => (prev + 1) % images.length);
+      setIndex(prev => (prev + 1) % adjustedImages.length);
     }, 5000);
 
     return () => clearInterval(interval);
   }, []);
+
+  useEffect(() => {
+    if (index !== adjustedImages.length - 1) return;
+
+    const timeout = setTimeout(() => {
+      setIndex(0);
+    }, 700);
+
+    return () => clearTimeout(timeout);
+  }, [index]);
 
   const goTo = (i: number) => {
     setIndex(i);
@@ -27,10 +39,12 @@ export const PictureSlider = () => {
   return (
     <div className={styles['pictures-slider']}>
       <div
-        className={styles['slider-track']}
+        className={cn(styles['slider-track'], {
+          [styles['no-transition']]: index === 0,
+        })}
         style={{ transform: `translateX(-${index * 100}%)` }}
       >
-        {images.map((src, i) => (
+        {adjustedImages.map((src, i) => (
           <div className={styles.slide} key={i}>
             <img src={src} alt={`slide=${i}`} />
           </div>
@@ -40,7 +54,10 @@ export const PictureSlider = () => {
         {images.map((_, i) => (
           <button
             key={i}
-            className={cn(styles.dot, { [styles.active]: i === index })}
+            className={cn(styles.dot, {
+              [styles.active]:
+                i === index || (index === adjustedImages.length - 1 && i === 0),
+            })}
             onClick={() => goTo(i)}
           />
         ))}
