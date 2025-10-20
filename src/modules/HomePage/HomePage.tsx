9dd5f59968fcf96f2665f39b56f5e1dfac10036d
wojciechprzyluski <przyluskiwojciech@gmail.com>
2025-10-20 18:36:11 +0200

    style&ref: homePage header vertical margins, delete unnecessary margin-inline typography

--- src/modules/HomePage/HomePage.tsx
+++ src/modules/HomePage/HomePage.tsx
@@ -1,11 +1,11 @@
-// import styles from './HomePage.module.scss';
+import styles from './HomePage.module.scss';
 
 import { PicturesSlider } from './components/PicturesSlider';
 
 export const HomePage = () => {
   return (
     <>
-      <h1>Welcome to Nice Gadgets store!</h1>
+      <h1 className={styles.title}>Welcome to Nice Gadgets store!</h1>
       <h1 className="visually-hidden">Product Catalog</h1>
 
       <PicturesSlider />
