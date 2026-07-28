729c02b5f46b68324474670097017084535b3f58
przwojwwp <przyluskiwojciech@gmail.com>
2026-07-28 22:26:49 +0200

    ref(ui): refine BackToTop styling/accessibility and remove unused slider icon assets

--- src/modules/HomePage/components/BrandNewModels/BrandNewModels.tsx
+++ src/modules/HomePage/components/BrandNewModels/BrandNewModels.tsx
@@ -1,15 +1,23 @@
 import { ProductCard } from '@/shared/components/ProductCard/ProductCard';
+
 import styles from './BrandNewModels.module.scss';
 
 export const BrandNewModels = ({ products }: { products: Product[] }) => {
   return (
     <section className={styles.brandNewModels}>
-      <header>
+      <header className={styles['section-header']}>
         <h2>
           Brand new <br />
           models
         </h2>
-        <div>left | right</div>
+        <div>
+          {/* <button type="button" aria-label="Previous">
+            <LeftIcon />
+          </button>
+          <button type="button" aria-label="Next">
+            <NextIcon />
+          </button> */}
+        </div>
       </header>
       <ProductCard products={products} />
     </section>
--- src/shared/components/BackToTop/BackToTop.tsx
+++ src/shared/components/BackToTop/BackToTop.tsx
@@ -1,18 +1,25 @@
-import styles from './BackToTop.module.scss';
 import BackToTopIcon from '@/assets/icons/arrow-up.svg?react';
+
+import styles from './BackToTop.module.scss';
+
 export const BackToTop = () => {
   const handleClick = () => {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
   };
 
   return (
     <button
       className={styles['back-to-top']}
-      onClick={handleClick}
       type="button"
+      onClick={handleClick}
+      aria-label="Back to top"
     >
       <span className={styles.label}>Back to top</span>
-      <span className={styles['icon-container']}>
+
+      <span className={styles['icon-container']} aria-hidden="true">
         <BackToTopIcon />
       </span>
     </button>
